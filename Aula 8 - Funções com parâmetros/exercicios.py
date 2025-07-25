6e34034f3488e45346999b4a4777d66b63cbfaf3
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-07-25 10:51:20 -0300

    Create exercicios.py

---
+++ Aula 8 - Funções com parâmetros/exercicios.py
@@ -0,0 +1,24 @@
+#Exercício 1: Crie uma função que receba dois números como parâmetros e retorne a soma deles.
+
+#Exercício 2: Crie uma função que receba uma lista como parâmetro e retorne o maior elemento da lista.
+
+#Exercício 3: Crie uma função que receba uma string e um caractere como parâmetros e retorne o número de vezes que o caractere aparece na string.
+
+#Exercício 4: Crie uma função que receba uma lista de números como parâmetro e retorne a média aritmética dos elementos.
+
+#Exercício 5: Crie uma função que receba uma lista de palavras como parâmetro e retorne a quantidade de palavras que começam com a letra "a".
+
+#Desafio 1) Criar uma função recursiva (que retorne ela mesma) para armazenar N termos da sequência de
+#Fibonacci em uma lista. N é definido pelo usuário. Ao encontrar os termos, imprimir a lista
+#e finalizar a função.
+
+#Desafio 2) Criar 5 funções: uma para um cadastro, outra para realizar o login, outra para mudar a senha,
+#outra para realizar logout e ainda uma para definir qual opção o usuário deseja escolher.
+#Utilize um loop while para sair do sistema apenas se o usuário desejar (criar a opção 'sair').
+#Atente-se às regras:
+#    - Só é possível realizar um cadastro se não houver nenhum anterior.
+#    - Só é possível realizar login se houver um cadastro.
+#    - Só é possível realizar login se o usuário informar corretamente username e senha.
+#    - Só é possível alterar a senha se o usuário estiver logado.
+#    - Só é possível alterar a senha se o usuário informar corretamente a senha atual.
+#    - Só é possível realizar logout se o usuário estiver logado.
