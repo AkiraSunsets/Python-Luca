55e76f373fbd706429bfa5074449f4942d51a029
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-07-28 11:11:41 -0300

    Create exercises.py

---
+++ Aula 9 - Args e Kwargs/exercises.py
@@ -0,0 +1,19 @@
+
+Exercício 1: Escreva uma função que receba um número variável de argumentos posicionais e retorne a soma de todos eles.
+
+Exercício 2: Escreva uma função que receba um número variável de argumentos nomeados e retorne um dicionário com esses argumentos.
+
+Exercício 3: Escreva uma função que receba um número variável de argumentos posicionais e nomeados e retorne a concatenação de todos eles.
+
+Exercício 4: Escreva uma função que receba um número variável de argumentos posicionais e retorne o maior deles.
+
+Exercício 5: Escreva uma função que receba um número variável de argumentos nomeados e retorne a média dos valores.
+
+Desafio 1 - Faça o sistema de uma corrida entre MerCúrio, Papa-Léguas, SoNic, FlaSh, LiGeirinho e 
+Super Homem (MC, PL, SN, FS, LG, SH). Crie uma função que receba os 6 corredores em ordem do
+vencedor até o último (pedir ao usuário), sendo os três primeiros em variáveis obrigatórias e 
+os três últimos em kwargs, com as chaves sendo as posições na corrida. Pedir ao usuário se houve 
+trapaça: 
+    - se não houve: apresentar a classificação final.
+    - se houve: pedir ao usuário quem trapaceou e quem foi prejudicado. Depois, trocá-los de 
+posições. Por fim, apresentar a classificação final.
